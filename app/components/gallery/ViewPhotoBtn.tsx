88e7747c635a2f650a54571856aa416e77d71842
ClaudyaSalim <claudya.salim@gmail.com>
2026-06-23 19:32:34 +0700

    Modify UI in gallery section

--- app/components/gallery/ViewPhotoBtn.tsx
+++ app/components/gallery/ViewPhotoBtn.tsx
@@ -1,15 +1,11 @@
 type ViewPhotoBtnProps = {
-//   topSm: string;
-//   leftSm: string;
-//   topMd: string;
-//   leftMd: string;
     position: string;
 };
 
 export default function ViewPhotoBtn(props: ViewPhotoBtnProps) {
   return (
     <button
-      className={`absolute bg-white text-black text-xs w-fit px-2 ${props.position} transform -translate-x-1/2 -translate-y-1/2 outline-6 outline-pink-200/60`}
+      className={`absolute bg-pink-100 text-black text-xs w-fit px-2 ${props.position} transform -translate-x-1/2 -translate-y-1/2 outline-6 outline-white/60`}
     >
       View
     </button>
