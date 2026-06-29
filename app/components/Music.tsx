7de36111c11f60ff583c4854f0fbc90bd219f1e9
ClaudyaSalim <claudya.salim@gmail.com>
2026-06-29 10:58:05 +0700

    Switch music implementation to MP3

--- app/components/Music.tsx
+++ app/components/Music.tsx
@@ -3,62 +3,20 @@ import { FiPause, FiPlay } from "react-icons/fi";
 
 type MusicProps = {
   playerRef: RefObject<any>;
-  shouldPlayRef: RefObject<any>;
   isPlaying: boolean;
-  onReady: (ready: boolean) => void;
   toggleMusic: () => void;
 };
 
 export default function Music({
   playerRef,
-  shouldPlayRef,
   isPlaying,
-  onReady,
   toggleMusic,
 }: MusicProps) {
-  const videoId = process.env.NEXT_PUBLIC_BGM_ID;
-  
-  useEffect(() => {
-    if (document.getElementById("yt-script")) {
-      return;
-    }
-
-    console.log("initializing player");
-    const ytubeScript = document.createElement("script");
-    ytubeScript.src = "https://www.youtube.com/iframe_api";
-    // ytubeScript.id = "yt-script"
-    document.body.appendChild(ytubeScript);
-
-    const initPlayer = () => {
-      console.log("initPlayer called");
-      playerRef.current = new (window as any).YT.Player("yt-player", {
-        videoId,
-        playerVars: { autoplay: 0, controls: 0, loop: 1, playlist: videoId },
-        events: {
-          onReady: () => {
-            onReady(true);
-            console.log("player ready");
-            if (shouldPlayRef.current) {
-              playerRef.current?.playVideo();
-            }
-          },
-        },
-      });
-    };
-
-    if ((window as any).YT?.Player) {
-      initPlayer();
-    } else {
-      (window as any).onYouTubeIframeAPIReady = initPlayer;
-    }
-  }, []);
+  const videoSrc = process.env.NEXT_PUBLIC_BGM_ID;
 
   return (
     <div className={`z-12 w-fit fixed right-0 bottom-4`}>
-      <div
-        id="yt-player"
-        className="w-1 h-1 opacity-0 pointer-events-none"
-      ></div>
+      <audio ref={playerRef} src={videoSrc} loop />
       <button
         onClick={toggleMusic}
         className="bg-primary rounded-l-full rounded-r-none"
--- app/sections/Cover.tsx
+++ app/sections/Cover.tsx
@@ -6,9 +6,7 @@ import EnvelopeCover from "./EnvelopeCover";
 
 export default function Cover() {
   const playerRef = useRef<any>(null);
-  const shouldPlayRef = useRef(false);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [ready, setReady] = useState(false);
 
   useEffect(() => {
     const hasOpened = sessionStorage.getItem("coverOpened");
@@ -22,31 +20,23 @@ export default function Cover() {
     }
   }, []);
 
-  useEffect(() => {
-    const handleVisibilityChange = () => {
-      if (document.visibilityState === "visible" && isPlaying) {
-        playerRef.current?.playVideo();
-      }
-    };
+  // useEffect(() => {
+  //   const handleVisibilityChange = () => {
+  //     if (document.visibilityState === "visible" && isPlaying) {
+  //       playerRef.current?.play();
+  //     }
+  //   };
 
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-    return () =>
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-  }, [isPlaying]);
+  //   document.addEventListener("visibilitychange", handleVisibilityChange);
+  //   return () =>
+  //     document.removeEventListener("visibilitychange", handleVisibilityChange);
+  // }, [isPlaying]);
 
   const toggleMusic = () => {
-    console.log("toggleMusic called");
-    console.log("ready:", ready);
-    console.log("playerRef.current:", playerRef.current);
-    console.log("isPlaying:", isPlaying);
-    if (!ready) {
-      shouldPlayRef.current = true;
-      return;
-    }
     if (isPlaying) {
-      playerRef.current.pauseVideo();
+      playerRef.current.pause();
     } else {
-      playerRef.current.playVideo();
+      playerRef.current.play();
     }
     setIsPlaying(!isPlaying);
   };
@@ -57,11 +47,7 @@ export default function Cover() {
         <EnvelopeCover toggleMusic={toggleMusic} />
         <Music
           playerRef={playerRef}
-          shouldPlayRef={shouldPlayRef}
           isPlaying={isPlaying}
-          onReady={(ready) => {
-            setReady(ready);
-          }}
           toggleMusic={toggleMusic}
         />
       </div>
